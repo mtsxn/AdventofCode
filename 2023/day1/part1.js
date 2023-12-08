9ef65f6b1997e34799326242fe0dfab01dbdaafd
mtsxn <149542654+mtsxn@users.noreply.github.com>
2023-12-08 18:58:26 +0100

    Create part1.js

---
+++ 2023/day1/part1.js
@@ -0,0 +1,23 @@
+  let tab = [
+        
+  ];
+  let sum = 0;
+
+  for (let i = 0; i < tab.length; i++) {
+    let firstDigit = null;
+    let lastDigit = null;
+  
+    for (let j = 0; j < tab[i].length; j++) {
+      if (/\d/.test(tab[i][j])) {
+        if (firstDigit === null) {
+          firstDigit = tab[i][j];
+        }
+        lastDigit = tab[i][j];
+      }
+    }
+  
+    if (firstDigit !== null && lastDigit !== null) {
+      sum += parseInt(firstDigit + lastDigit);
+    }
+  }
+  console.log(sum);
