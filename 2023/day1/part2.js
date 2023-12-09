6f5cb01f824c9aa9b4ca368dafe36e81c0f6e152
mtsxn <149542654+mtsxn@users.noreply.github.com>
2023-12-09 14:57:11 +0100

    Create part2.js

---
+++ 2023/day1/part2.js
@@ -0,0 +1,20 @@
+  function extractNumbers(tab) {
+    const lines = [
+      ["oneight", 18],
+      ["twone", 21],
+      ["threeight", 38],
+      ["fiveight", 58],
+      ["sevenine", 79],
+      ["eightwo", 82],
+      ["eighthree", 83],
+      ["nineight", 98],
+      ["one", 1],
+      ["two", 2],
+      ["three", 3],
+      ["four", 4],
+      ["five", 5],
+      ["six", 6],
+      ["seven", 7],
+      ["eight", 8],
+      ["nine", 9],
+    ];
