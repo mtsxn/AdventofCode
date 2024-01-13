9fb284a7840047964cea2a1adf747b8bc3a882be
mtsxn <149542654+mtsxn@users.noreply.github.com>
2024-01-13 23:14:44 +0100

    Update part2.js

--- 2023/day2/part2.js
+++ 2023/day2/part2.js
@@ -1,3 +1,14 @@
 const input = [
-
   ];
+
+function part2() {
+  answer = 0;
+  inputs.forEach(x => {
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+    
+    //puzzle is still simple, let's just split on spaces
+    let y = x.split(' ');
+  });
+}
